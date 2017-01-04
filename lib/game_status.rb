9dd68361ab252299b8dc0ecda9fef518ca6e063e
David Ordovskiy <david.ordovsky@gmail.com>
2017-01-04 13:21:14 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,29 +13,39 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
 ]
 
+# checking winning combinations vs places on the board.
+# the first combination with either all X or all O will be returned
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] && board[combo[0]] == board[combo[2]] && position_taken?(board, combo[0])
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[0]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
+# checking if all board positions are either X or O
 def full?(board)
-  return false if board.any? { |position| position == " " }
-  draw?(board)
+  board.all? do |position|
+    position == 'X' || position == 'O'
+  end
 end
 
+# checking if the game is won or if the board is full
 def draw?(board)
-  true if won?(board) == nil
+  !won?(board) && full?(board)
 end
 
+# checking if the game is won or there's a draw
 def over?(board)
-  return false if board.any? { |position| position == " " }
   draw?(board) || won?(board)
 end
 
+# 1. grabbing the winning combo, if there is one
+# 2. if there is, returning the first element of the combo
+# (we've already made sure all elements of it are either X's or O's)
 def winner(board)
   combo = won?(board)
-  unless combo == nil
-    winner = ( combo.all? { |index| board[index] == 'X' } ? 'X' : 'O' )
+  if combo
+    board[combo[0]]
   end
 end
