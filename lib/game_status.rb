65375b2f55a13c02e4447d70fb0db3470e3bca26
Cleavon Brathwaite <cbrathwaite90@gmail.com>
2017-01-15 19:25:41 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,77 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS= [
+top_row = [0,1,2],#top row
+   [3,4,5],#middle row
+   [6,7,8],#bottow row
+   [0,4,8],#diagonal1
+   [2,4,6],#diagonal2
+   [0,3,6],#line1
+   [1,4,7],#line2
+   [2,5,8]#line3
+]
+# who won?
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combination
+  else
+      false
+    end
+  end
+end
+
+def full?(board)
+  board.all? do |pos|
+    pos == "X" || pos =="O"
+  end
+end
+
+def draw?(board)
+  if !(won?(board)) && full?(board)
+    return true
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+
+def winner(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return "X"
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return "O"
+  else
+      nil
+    end
+  end
+
+end
