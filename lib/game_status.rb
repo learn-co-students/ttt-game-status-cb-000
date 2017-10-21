99e2485dc20ecb166e7e87f98c4472f90fff5832
abdillahi09 <abdillahi.muse@hotmail.com>
2017-10-21 15:21:05 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,57 +16,63 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if !board.any?{|i| i != " "}
-    return false
-  else
-    WIN_COMBINATIONS.each do |combination|
-      combination
-      win_index1 = combination[0]
-      win_index2 = combination[1]
-      win_index3 = combination[2]
+  WIN_COMBINATIONS.detect do | extract |
 
-      position1 = board[win_index1]
-      position2 = board[win_index2]
-      position3 = board[win_index3]
+    win_index_1 = extract[0]
+    win_index_2 = extract[1]
+    win_index_3 = extract[2]
 
-      if (position1 == "X" && position2 == "X" && position3 == "X") ||
-         (position1 == "O" && position2 == "O" && position3 == "O")
-        return combination
-      end
-    end
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+if position_taken?(board, win_index_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3)
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return extract
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return extract
+    elsif board[WIN_COMBINATIONS[6][0]] == "X" && board[WIN_COMBINATIONS[6][1]] == "X" && board[WIN_COMBINATIONS[6][2]] == "X"
+      return [0,4,8]
+    elsif board[WIN_COMBINATIONS[6][0]] == "O" && board[WIN_COMBINATIONS[6][1]] == "O" && board[WIN_COMBINATIONS[6][2]] == "O"
+      return [0,4,8]
+    elsif board[WIN_COMBINATIONS[7][0]] == "X" && board[WIN_COMBINATIONS[7][1]] == "X" && board[WIN_COMBINATIONS[7][2]] == "X"
+      return [2,4,6]
+    elsif board[WIN_COMBINATIONS[7][0]] == "O" && board[WIN_COMBINATIONS[7][1]] == "O" && board[WIN_COMBINATIONS[7][2]] == "O"
+      return [2,4,6]
+    else
+      return false
   end
-  return false
 end
+end
+end
+
 
 def full?(board)
-  return board.include?(" ")? false : true
+  board.all?{|filled| filled == "X" || filled == "O"}
 end
+
 
 def draw?(board)
-  if won?(board) == false && full?(board)
-    return true
-  elsif won?(board) == false && full?(board)
-    return false
-  elsif won?(board) != false
-    return false
+  if !won?(board) && full?(board)
+    true
   end
 end
+
 
 def over?(board)
-  return (won?(board) || draw?(board) || full?(board))? true : false
-end
-
-def winner?(board)
-  if combination = won?(board)
-    board[combination.first]
+  if won?(board) || draw?(board) || full?(board)
+    true
   end
 end
-# def winner?(board)
-#   if won?(board)
-#     winning_combination = won?(board)
-#     player = board[winning_combination[0]]
-#     return player
-#   else
-#     return nil
-#   end
-# end
+
+def winner(board)
+  wincombo = won?(board)
+  if wincombo === nil
+    return nil
+  elsif board[wincombo[0]] == "X"
+    return "X"
+  elsif board[wincombo[0]] == "O"
+    return "O"
+  end
+end
